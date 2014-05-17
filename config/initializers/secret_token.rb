eefe3a8750c7fe08a74c0227fbaa078a26f56b82
Josue Palmerin <jpalmer2@stedwards.edu>
2014-05-17 02:51:08 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LocoApp::Application.config.secret_key_base = '9af1853907b4cf304c37acaae51b92a9a109a2063302b0b52a1c7f3c603498a53826ed3bbe9b71f33bf6c75b41095c542827b211c8835e47ee6824e42cb70e40'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LocoApp::Application.config.session_store :cookie_store, key: '_loco_app_session'
